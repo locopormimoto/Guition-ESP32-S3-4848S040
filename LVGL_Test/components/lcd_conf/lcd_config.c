6658f2cbda7265100802d88eb3bb9b60e995ec39
Antonio R <a_r@example.com>
2026-03-25 18:11:01 +0100

    lcd hw config code moved to dedicated folder

---
+++ LVGL_Test/components/lcd_conf/lcd_config.c
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include "lcd_config.h"
+
+/* This file contains LCD hardware configuration macros for ESP32-S3
+ * Pin definitions, clock settings, and resolution parameters are defined in lcd_config.h
+ */
--- LVGL_Test/main/main.c
+++ LVGL_Test/main/main.c
@@ -24,51 +24,10 @@
 #include "esp_err.h"
 #include "esp_log.h"
 #include "esp_lcd_st7701.h"
-//#include "demos/lv_demos.h"
 #include "ui.h"
+#include "lcd_config.h"
 
 static const char *TAG = "Main";
-
-#define LCD_PIXEL_CLOCK_HZ     (12 * 1000 * 1000)
-#define LCD_BK_LIGHT_ON_LEVEL  1
-#define LCD_BK_LIGHT_OFF_LEVEL !LCD_BK_LIGHT_ON_LEVEL
-#define PIN_NUM_BK_LIGHT       38
-#define PIN_NUM_HSYNC          16
-#define PIN_NUM_VSYNC          17
-#define PIN_NUM_DE             18
-#define PIN_NUM_PCLK           21
-#define PIN_NUM_DATA0          4  // B0
-#define PIN_NUM_DATA1          5  // B1
-#define PIN_NUM_DATA2          6  // B2
-#define PIN_NUM_DATA3          7  // B3
-#define PIN_NUM_DATA4          15 // B4
-#define PIN_NUM_DATA5          8  // G0
-#define PIN_NUM_DATA6          20 // G1
-#define PIN_NUM_DATA7          3  // G2
-#define PIN_NUM_DATA8          46 // G3
-#define PIN_NUM_DATA9          9  // G4
-#define PIN_NUM_DATA10         10 // G5
-#define PIN_NUM_DATA11         11 // R0
-#define PIN_NUM_DATA12         12 // R1
-#define PIN_NUM_DATA13         13 // R2
-#define PIN_NUM_DATA14         14 // R3
-#define PIN_NUM_DATA15         0  // R4
-#define PIN_NUM_DISP_EN		   -1
-
-#define PIN_NUM_SPI_MOSI 		47
-#define PIN_NUM_SPI_CLK  		48
-#define PIN_NUM_SPI_CS   		39
-
-#define LCD_H_RES              480
-#define LCD_V_RES              480
-
-#if CONFIG_DOUBLE_FB
-#define LCD_NUM_FB             2
-#else
-#define LCD_NUM_FB             1
-#endif
-
-#define LVGL_TICK_PERIOD_MS    1
 
 #if CONFIG_AVOID_TEAR_EFFECT_WITH_SEM
 SemaphoreHandle_t sem_vsync_end;
